200d7da48fa80b1a87f7264434df69cd887d6261
Travis Highley <betterthanapigeon@gmail.com>
2018-06-25 03:48:08 -0400

    added vueds

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import layout from './layout'
 import router from './router'
 import store from './store'
+import system from 'trv-vueds'
+import 'trv-vueds/dist/system.css'
+
+Vue.use(system)
 
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-// placeholder
+module.exports = {
+  // lintOnSave: true,
+  css: {
+    extract: true,
+    sourceMap: true,
+    loaderOptions: {
+      sass: {
+        data: '@import "@/assets/scss/index.scss";'
+      }
+    }
+  }
+}
