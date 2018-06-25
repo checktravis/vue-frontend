aab5dc1fd53cf8716c4f8bdf12efff62f70c02e5
Travis Highley <betterthanapigeon@gmail.com>
2018-06-25 00:18:25 -0400

    reset css

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,1 @@
-// import jsonImporter from 'node-sass-json-importer';
-const jsonImporter = require('node-sass-json-importer')
-
-
-module.exports = {
-  // lintOnSave: true,
-  css: {
-    extract: true,
-    sourceMap: true,
-    loaderOptions: {
-      sass: {
-        data: '@import "@/assets/scss/index.scss";',
-        importer: jsonImporter
-      }
-    }
-  }
-}
+// placeholder
